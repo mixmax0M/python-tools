b5834f5bfea72f0a31f15d20b519f959d38a01ac
mixmax0M <50010733+mixmax0M@users.noreply.github.com>
2021-08-17 21:14:37 +0200

    Update Kahooter.py

--- Kahooter.py
+++ Kahooter.py
@@ -7,17 +7,16 @@ def GetGame(id):
     r = requests.get(f"https://kahoot.it/reserve/session/{id}")
     return r
 
-def GetCode():
+def GetCode(rangue):
     num = ""
-    for i in range(6):
+    for i in range(rangue):
         num = num + random.choice(string.digits)
         i =+ 1
     return num
 
 def Finder():
-    sleep(0.4)
-
-    code = GetCode()
+    sleep(0.04)
+    code = GetCode(7)
     reasons = GetGame(code).reason
     stcode = GetGame(code).status_code
 
@@ -26,11 +25,29 @@ def Finder():
         time = datetime.now().strftime("%H:%M:%S")
         f = open("kcodes.txt", "a")
         f.write(f"{code} Time -> {time}\n")
+    elif stcode == 503:
+        sleep(0.5)
     else:
-        print("Code: " + code + " failed. Status code -> " + str(stcode) + " Reason -> " + reasons)
+        pass
+
+def Minder():
+    code = GetCode(6)
+    reasons = GetGame(code).reason
+    stcode = GetGame(code).status_code
+
+    if reasons == "OK":
+        print("Working code found! -> " + code + f" Reason -> {reasons}")
+        time = datetime.now().strftime("%H:%M:%S")
+        f = open("kcodes.txt", "a")
+        f.write(f"{code} Time -> {time}\n")
+    elif stcode == 503:
+        sleep(0.5)
+    else:
+        pass
 
 threads = int(input("Input the amount of threads: "))
 
 while True:
     with concurrent.futures.ThreadPoolExecutor() as executer:
         results = [executer.submit(Finder) for _ in range(threads)]
+        mesults = [executer.submit(Minder) for _ in range(threads)]
