cbeecfcbb03f454c7721f3f431c7e90521e4bf21
mixmax0M <50010733+mixmax0M@users.noreply.github.com>
2022-01-02 10:43:13 +0100

    Update CalorieCalc.py

--- CalorieCalc.py
+++ CalorieCalc.py
@@ -1,25 +1,43 @@
+from os import system
 from time import sleep
 
-def main():
-    print('========================')
-    print('Calorie Calculator\n')
-    print('Gender')
-    print('[1] Male')
-    print('[2] Female\n')
-    gender = int(input('Selection: '))
-    age = float(input('Enter your age: '))
-    weight = float(input('Enter your weight in kg: '))
-    height = float(input('Enter your height in cm: '))
+print('========================')
+print('Calorie Calculator\n')
+print('Gender')
+print('[1] Male')
+print('[2] Female')
+gender = int(input('\nSelection: '))
+print('========================')
 
-    cal = 0
+print('System')
+print('[1] Metric')
+print('[2] Imperial')
+syst = int(input('\nSelection: '))
+print('========================')
+age = float(input('\nEnter your age: '))
+print('========================')
 
+if syst == 1:
+    weight = float(input('Enter your weight: '))
+    height = float(input('Enter your height: '))
+    
+elif syst == 2:
+    lbs = float(input('Enter your weight: '))
+    ft = float(input('Enter your height (ft): '))
+    inc = float(input('Enter your height (in): '))
+    
+    weight = lbs / 2.205 
+    height = ft * 30.48 + inc + 2.54
+else:
+    print("Invalid Input. Closing in 3 seconds")
+    sleep(3)
+    exit
+
+def main(gender, weight, height, age):
     if gender == 1:
         cal = 66.47 + (13.7*weight) + (5.0*height) - (6.8*age)
     elif gender == 2:
         cal = 655.1 + (9.6*weight) + (1.8*height) - (4.7*age)
-    else:
-        print('Invalid Input\n')
-        main()
 
     print('========================')
     print('Activity')
@@ -32,22 +50,25 @@ def main():
 
     activitys = int(input('Selection: '))
 
-    match (activitys):
-        case 1:
-            cal = cal * 1
-        case 2:
-            cal = cal * 1.2
-        case 3:
-            cal = cal * 1.375
-        case 4:
-            cal = cal * 1.55
-        case 5:
-            cal = cal * 1.725
-        case 6:
-            cal = cal * 1.9
-        case _:
-            cal = cal * 1
-            print('Invalid Selection')
+    if activitys == 1:
+        cal = cal * 1
+    elif activitys == 2:
+        cal = cal * 1.2
+    elif activitys == 3:
+        cal = cal * 1.375
+    elif activitys == 4:
+        cal = cal * 1.55
+    elif activitys == 5:
+        cal = cal * 1.725
+    elif activitys == 6:
+        cal = cal * 1.9
+    else:
+        cal = cal * 1
+        print('Invalid Selection.')
 
     print('========================')
     print(f'Your basal metabolic rate is {cal}')
+    print('========================')
+    input()
+
+main(gender, weight, height, age)
