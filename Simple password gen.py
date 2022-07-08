d588b291fa5fe1617ff21ecadc65bec95d459e43
mixmax0M <50010733+mixmax0M@users.noreply.github.com>
2022-07-08 12:46:29 +0200

    Update Simple password gen.py

--- Simple password gen.py
+++ Simple password gen.py
@@ -2,11 +2,8 @@ import random
 
 chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzöäüß1234567890!"§$%&/()=?-.,;_:@#<>^°²³{[]}\\'
 
-numbers = input('How many passwords would you like to generate?\n')
-numbers = int(numbers)
-
-length = input('Password strenght? Type Numbers \n')
-length = int(length)
+numbers = int(input('How many passwords would you like to generate?\n'))
+length = int(input('Password strenght? Type Numbers \n'))
 
 for n in range(numbers):
     passw = ''
